7b68c6695168f9ac5d6210b01d33c958fc37cb7a
moriartyMLR <97626090+moriartyMLR@users.noreply.github.com>
2022-07-07 19:26:42 -0400

    Developing logic

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 #Project One: Caesar Cipher
 
-def caesar_cipher(steps, message):
+def caesar_cipher(message):
+    #ascii value number
     old = 0
+    #loop through string/message, store ascii num and add steps
     for l in message:
         old = ord(l)
+        for i in range(3):
+            if old == 122:
+                old = 0
+                continue
+            old += 1
+        print(chr(old))
 
 
-caesar_cipher((2), ('bad'))
+caesar_cipher('bax')
