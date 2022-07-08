2d33ec8bcec7d0814b63fcabce1fca2930d2c44d
moriartyMLR <97626090+moriartyMLR@users.noreply.github.com>
2022-07-08 17:41:39 -0400

    Functional logic
    Function can encrypt but only going clockwise

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 #Project One: Caesar Cipher
 
-def caesar_cipher(message):
+def caesar_cipher(message, steps):
     #ascii value number
     old = 0
     #loop through string/message, store ascii num and add steps
     for l in message:
         old = ord(l)
-        for i in range(3):
+        for i in range(steps):
             if old == 122:
-                old = 0
-                continue
+                old = 97
             old += 1
-        print(chr(old))
+        print(chr(old), end="")
+    print("")
 
 
-caesar_cipher('bax')
+caesar_cipher('baape', 2)
