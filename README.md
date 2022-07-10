bf42024b04e941b4a49c99e8b042aa90f3607ca8
moriartyMLR <97626090+moriartyMLR@users.noreply.github.com>
2022-07-10 19:56:06 -0400

    Finished logic, finished command-line interface
    'Encrypt' and 'decrypt' functions made. Basic terminal interface

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CaesarsCipher
- Caesar Cipher Terminal App
+ Very Basic Caesar Cipher Terminal App
+ First time using GitHub and my first project outside of tutorials 
+
+INSTRUCTIONS 
+    * Choose whether to "encrypt" or "decrypt" some text
+    * Enter your message to be encrypted or decrypted
+    * Choose the amount of times each letter gets shifted foreward (or backwards if you chose "decrypt")
+    * And that's it!
+
--- app.py
+++ app.py
@@ -1,17 +1,51 @@
 #Project One: Caesar Cipher
+class CaesarCipher:
+    def encrypter(message, steps):
+        #ascii value number and "encrypted" message placeholder
+        old = 0
+        encrypted = ""
+        #loop through string/message, store ascii num and add shifts
+        for l in message:
+            old = ord(l)
+            for i in range(steps):
+                if old == 122:
+                    old = 97
+                old += 1
+             #update new message placeholder
+            encrypted = encrypted + chr(old)
+        print(f"Encrypted message using {steps} shifts: {encrypted}")
 
-def caesar_cipher(message, steps):
-    #ascii value number
-    old = 0
-    #loop through string/message, store ascii num and add steps
-    for l in message:
-        old = ord(l)
-        for i in range(steps):
-            if old == 122:
-                old = 97
-            old += 1
-        print(chr(old), end="")
-    print("")
+    def decrypter(message, steps):
+        #ascii value number and "decrypted" message placeholder
+        old = 0
+        decrypted = ""
+        #loop through string/message, store ascii num and add shifts to it's value
+        for l in message:
+            old = ord(l)
+            for i in range(steps):
+                if old == 97:
+                    old = 122
+                old -= 1
+            #update new message placeholder
+            decrypted = decrypted + chr(old)
+        print(f"Decrypted message using {steps} shifts: {decrypted}")
 
+def main():
+    while True:
+        u_input = input("Would You Like to (E)ncrypt, (D)ecrypt, or (Q)uit: ").lower()
+        if u_input == "q":
+            quit()
+        elif u_input == "e":
+            try:
+                CaesarCipher.encrypter(str(input("Enter your message to be encrypted: ")), int(input("Shift letters by: ")))
+            except:
+                print("Please enter a number for shifts")
+        elif u_input == "d":
+            try:
+                CaesarCipher.decrypter(str(input("Enter your message to be decrypted: ")), int(input("Shift letters by: ")))
+            except:
+                print("Please enter a number for shifts")
+        elif u_input != "e" or "d":
+            print("Select 'e', 'd', or 'q' please")
 
-caesar_cipher('baape', 2)
+main()
